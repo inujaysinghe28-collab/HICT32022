bb7489ec0ac79a9fa378743d7d278faad66df7e2
inujayasinghe <inujaysinghe28@gmail.com>
2026-07-08 21:46:22 +0530

    Week 2

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mockData = require('./data'); // Importing our data file
+const app = express ();
+const PORT = 3000;
+
+
+app.get ('/', (request, response) => {
+response.send("Hello! My Node.js server is running!");
+});
+
+app.get ('/api/users', (request, response) => {
+response.json(mockData);
+}) ;
+
+app.listen(PORT, () => {
+    console.log('Server is listening on http://localhost:${PORT}');
+});
