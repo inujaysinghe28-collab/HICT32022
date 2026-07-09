5448a704ae86672a5cb74ae1ad69579ed0897c05
inujayasinghe <inujaysinghe28@gmail.com>
2026-07-09 16:37:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 const express = require('express');
-const mockData = require('./data'); // Importing our data file
-const app = express ();
+const mockData = require('./data'); 
+const app = express();
 const PORT = 3000;
 
-
-app.get ('/', (request, response) => {
-response.send("Hello! My Node.js server is running!");
+app.get('/', (request, response) => {
+    response.send("Hello! My Node.js server is running!");
 });
 
-app.get ('/api/users', (request, response) => {
-response.json(mockData);
-}) ;
+app.get('/api/users', (request, response) => {
+    response.json(mockData);
+});
 
 app.listen(PORT, () => {
-    console.log('Server is listening on http://localhost:${PORT}');
+    console.log(`Server is listening on http://localhost:${PORT}`);
 });
